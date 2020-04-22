23e367bd4f5c1e8a52da2b3c01c79b15f3e621cc
StarPlatinum-17 <ramirezoliver.g@gmail.com>
2020-04-23 01:26:14 +0800

    implement QuickSort

--- sorting_algo.js
+++ sorting_algo.js
@@ -2,10 +2,10 @@ const BACKGROUND_FILL = 50;
 const COMPARE_FILL = '#8B0000';
 const DEFAULT_FILL = 255;
 
-const CANVAS_LENGTH = 1201
-const CANVAS_HEIGHT = 400
+const CANVAS_LENGTH = 1201;
+const CANVAS_HEIGHT = 400;
 
-var NUM_ELEMENTS = 112
+var NUM_ELEMENTS = 112;
 
 let i = 0;
 
@@ -35,6 +35,11 @@ function draw(){
     }
 }
 
+//buttons
+function trigger_QuickSort(){
+    QuickSort(_elements,0,_elements.length-1);
+}
+
 
 //Functionality
 function generate_array(){
@@ -48,4 +53,53 @@ function generate_array(){
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function verify_sorted(arr) {
+    var is_sorted = true;
+
+    for (let i = 1; i < arr.length; i++){
+        if (arr[i-1] > arr[i]){
+            is_sorted = false;
+            break;
+        } 
+    }
+    return is_sorted;
+}
+
+
+//quick sort
+function qs_partition(arr, start, end){
+   
+    let j = start;
+    let tmp;
+    for (let i = start; i < end; i++){
+        if (arr[i] < arr[end]){
+            
+            //swap i and j values
+            tmp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = tmp
+
+
+            j = j+1;
+        }
+
+    }
+    //swap pivot and j values
+    tmp = arr[j]
+    arr[j] = arr[end]
+    arr[end] = tmp
+
+    return j
+}
+
+function QuickSort(arr, start, end){
+    if (start < end){
+
+        let q = qs_partition(arr, start, end);
+
+        QuickSort(arr,start,q-1);
+        QuickSort(arr,q+1,end);
+    }
 }
--- tests/tests.js
+++ tests/tests.js
@@ -5,6 +5,59 @@ QUnit.test("Generate Array Length Test", function(assert){
     assert.equal(_elements.length,NUM_ELEMENTS);
 });
 
-QUnit.test("QUnit is sequential", function(assert){
-    assert.equal(_elements.length,NUM_ELEMENTS);
+QUnit.test("Check sorted arrays", function(assert){
+    var arr = [2,3,5,6,9];
+    var result = verify_sorted(arr)
+
+    assert.ok(result);
+
+    arr = [2,3,9,9,5];
+    result = verify_sorted(arr)
+
+    assert.notOk(result);
 });
+
+QUnit.test("Test qs_partition Single element", function(assert){
+    var arr  = [1,2,3];
+
+    let result = qs_partition(arr, 0, 0);
+
+    assert.equal(result,0,"return only index");
+    assert.deepEqual(arr,[1,2,3], "no change in array");
+
+    result = qs_partition(arr, 1, 1);
+
+    assert.equal(result,1,"return only index");
+    assert.deepEqual(arr,[1,2,3], "no change in array");
+});
+
+QUnit.test("Test qs_partition pivot value", function(assert){
+    // for now pivot is at the end of array
+    var arr  = [2,3,9,9,5];
+
+    let i_start = 0;
+    let i_end = 4;
+    
+
+    let i_result = qs_partition(arr, i_start, i_end);
+    assert.equal(i_result,2, "correct parition index");
+
+    assert.ok(arr.slice(i_result,i_end).every(el => el >= arr[i_result]), "every element right is greater than pivot")
+    assert.ok(arr.slice(i_start,i_result).every(el => el <= arr[i_result]), "every element left is less than pivot")
+
+    arr = [2,3,5,8,3,5,6,3,67,9,3]
+    i_result = qs_partition(arr, 0, (arr.length-1));
+    assert.ok(arr.slice(i_result,i_end).every(el => el >= arr[i_result]), "every element right is greater than pivot")
+    assert.ok(arr.slice(i_start,i_result).every(el => el <= arr[i_result]), "every element left is less than pivot")
+
+});
+
+
+QUnit.test("Test QuickSort if sorted", function(assert){
+    var arr  = [3,9,9,2,5];
+    var sorted_arr = [2,3,5,9,9];
+
+    QuickSort(arr,0,arr.length -1);
+    assert.deepEqual(arr,sorted_arr, "check array");    
+});
+
