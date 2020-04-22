d6472e0f84fd1d959d51de4e3289c668777582ca
StarPlatinum-17 <ramirezoliver.g@gmail.com>
2020-04-23 02:36:25 +0800

    added _states for completed sort

--- sorting_algo.js
+++ sorting_algo.js
@@ -1,12 +1,13 @@
 const BACKGROUND_FILL = 50;
 const COMPARE_FILL = '#8B0000';
 const DEFAULT_FILL = 255;
+const SORTED_FILL = '#99ff33';
 
 const CANVAS_LENGTH = 1201;
 const CANVAS_HEIGHT = 400;
 
 var NUM_ELEMENTS = 112;
-
+_states = _elements = []
 let i = 0;
 
 function setup(){
@@ -18,32 +19,40 @@ function setup(){
 
 //Visualization
 function draw(){
-    console.log(_elements)
-    if (Array.isArray(_elements) && _elements.length){
-        background(BACKGROUND_FILL);
+    background(BACKGROUND_FILL);
         
-        w = width/(_elements.length + 1)
-        m = (width - w * _elements.length) / 2
+    w = width/(_elements.length + 1)
+    m = (width - w * _elements.length) / 2
         
-        for (let i = 0; i < _elements.length; i++){
-            
-            stroke(0);
+    for (let i = 0; i < _elements.length; i++){
+        stroke(0);
+
+        if (_states[i] == 1){
+            fill(SORTED_FILL);
+
+        } else{
             fill(DEFAULT_FILL);
-            rect(i * w + m, height - _elements[i], w, height);
-   
         }
+            
+
+
+        rect(i * w + m, height - _elements[i], w, height);
+   
     }
+
 }
 
 //buttons
 function trigger_QuickSort(){
     QuickSort(_elements,0,_elements.length-1);
+    verify_sorted(_elements);
 }
 
 
 //Functionality
 function generate_array(){
     _elements = Array(NUM_ELEMENTS)
+    _states = Array(NUM_ELEMENTS).fill(0)
     
     for (let i = 0; i < _elements.length; i++){
         _rand = Math.random()
@@ -57,13 +66,19 @@ function sleep(ms) {
 
 function verify_sorted(arr) {
     var is_sorted = true;
-
+    
     for (let i = 1; i < arr.length; i++){
         if (arr[i-1] > arr[i]){
+
             is_sorted = false;
+            
             break;
-        } 
+        } else{
+            _states[i-1] = 1;
+
+        }
     }
+    _states[arr.length-1    ] = 1;
     return is_sorted;
 }
 
