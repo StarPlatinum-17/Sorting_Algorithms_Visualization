d6bcf1126bad6eccc252acd321eb6fc2aa547566
StarPlatinum-17 <ramirezoliver.g@gmail.com>
2020-04-26 03:38:40 +0800

    update animation colors

--- sorting_algo.js
+++ sorting_algo.js
@@ -1,11 +1,16 @@
 const BACKGROUND_FILL = 50;
-const COMPARE_FILL = '#8B0000';
 const DEFAULT_FILL = 255;
+const PIVOT_COLOR = '#cc0000';
+const QS_LESS = '#E5FF24';
+const QS_MORE = '#6CFF33';
+const SORTED_FILL = '#BFB7BF';
+
+const BEAT_MS =10;
 
 const CANVAS_LENGTH = 1201;
 const CANVAS_HEIGHT = 400;
 
-var NUM_ELEMENTS = 112;
+var NUM_ELEMENTS = 1000;
 
 _elements = _states = [];
 
@@ -15,7 +20,7 @@ function setup(){
     let cnv = createCanvas(CANVAS_LENGTH, CANVAS_HEIGHT);
     cnv.parent("sketch");
     background(BACKGROUND_FILL);
-    frameRate(10);
+    frameRate(60);
 }
 
 //Visualization
@@ -26,12 +31,22 @@ function draw(){
 
     w = width/(_elements.length + 1)
     m = (width - w * _elements.length) / 2
-        
+    
+    noStroke();
     for (let i = 0; i < _elements.length; i++){
-        stroke(0);
+        
 
         if (_states[i] == 1){
             fill(SORTED_FILL);
+
+        } else if (_states[i] == 2){
+            fill(PIVOT_COLOR);
+
+        } else if (_states[i] == 3){
+            fill(QS_LESS);
+
+        } else if (_states[i] == 4){
+            fill(QS_MORE);
 
         } else{
             fill(DEFAULT_FILL);
@@ -49,18 +64,39 @@ function trigger_QuickSort(){
 
 
 //Functionality
-function generate_array(){
-    _elements = Array(NUM_ELEMENTS)
-    
+function generate_array(num_elements = NUM_ELEMENTS, max_value = CANVAS_HEIGHT){
+    _elements = Array(num_elements);
+    _states = Array(num_elements);
     for (let i = 0; i < _elements.length; i++){
-        _rand = Math.random()
-        _elements[i] = _rand*(CANVAS_HEIGHT) + 20*(1-_rand);
+        _rand = Math.random();
+        _elements[i] = _rand*(max_value) + 20*(1-_rand);
     }
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+function swap(arr,i,j){
+    let tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+    return arr;
+}
+
+function _clear(arr,start,end){
+    for (let i = start ; i <= end; i++){
+        arr[i]=0;
+    }
+}
+
+function set_arr_with_value(arr,indexes,value){
+    for (let i = 0 ; i < indexes.length; i++){
+        let j=indexes[i];
+        arr[j] = value;
+    }
+}
+
 
 function verify_sorted(arr) {
     var is_sorted = true;
@@ -76,37 +112,50 @@ function verify_sorted(arr) {
 
 
 //quick sort
-function qs_partition(arr, start, end){
+async function qs_partition(arr, start, end){
    
     let j = start;
     let tmp;
+    set_arr_with_value(_states,[end],2)
     for (let i = start; i < end; i++){
+        set_arr_with_value(_states,[i],2)
+        await sleep(BEAT_MS);
+        
         if (arr[i] < arr[end]){
             
             //swap i and j values
-            tmp = arr[i]
-            arr[i] = arr[j]
-            arr[j] = tmp
+            swap(arr,i,j);
+            swap(_states,i,j);
 
-
+            await sleep(BEAT_MS);
+            set_arr_with_value(_states,[j],4)
+            
             j = j+1;
+        } else{
+            await sleep(BEAT_MS);
+            set_arr_with_value(_states,[i],3)
         }
 
     }
     //swap pivot and j values
-    tmp = arr[j]
-    arr[j] = arr[end]
-    arr[end] = tmp
-
-    return j
+    set_arr_with_value(_states,[end],1);
+    swap(arr,j,end);
+    await sleep(BEAT_MS);
+    _clear(_states,start,end);
+    set_arr_with_value(_states,[j],1);
+    return j;
+    
 }
 
-function QuickSort(arr, start, end){
-    if (start < end){
+async function QuickSort(arr, start, end){
+    if (start <= end){
 
-        let q = qs_partition(arr, start, end);
+        let q = await qs_partition(arr, start, end);
 
-        QuickSort(arr,start,q-1);
-        QuickSort(arr,q+1,end);
+        await Promise.all([
+        QuickSort(arr,start,q-1),
+        QuickSort(arr,q+1,end)
+        ])
+
     }
 }
--- tests/tests.js
+++ tests/tests.js
@@ -2,8 +2,8 @@ console.log('qunit tests start');
 
 
 QUnit.test("Generate Array Length Test", function(assert){
-    generate_array();
-    assert.equal(_elements.length,NUM_ELEMENTS);
+    generate_array(112);
+    assert.equal(_elements.length,112);
 });
 
 QUnit.test("Check sorted arrays", function(assert){
@@ -18,21 +18,39 @@ QUnit.test("Check sorted arrays", function(assert){
     assert.notOk(result);
 });
 
-QUnit.test("Test qs_partition Single element", function(assert){
+QUnit.test("Swap element values", function(assert){
+    var arr = [1,2,5,3,6,7];
+
+    swap(arr,0,5);
+    assert.deepEqual(arr,[7,2,5,3,6,1],"swap values ok");
+
+});
+
+
+QUnit.test("Swap element values", function(assert){
+    var arr = [1,2,5,3,6,7];
+
+    set_arr_with_value(arr,[1,4],0);
+    assert.deepEqual(arr,[1,0,5,3,0,7],"set values ok");
+});
+
+
+QUnit.test("Test qs_partition Single element", async function(assert){
     var arr  = [1,2,3];
 
-    let result = qs_partition(arr, 0, 0);
+    let result = await qs_partition(arr, 0, 0);
+    
 
     assert.equal(result,0,"return only index");
     assert.deepEqual(arr,[1,2,3], "no change in array");
 
-    result = qs_partition(arr, 1, 1);
+    result = await qs_partition(arr, 1, 1);
 
     assert.equal(result,1,"return only index");
     assert.deepEqual(arr,[1,2,3], "no change in array");
 });
 
-QUnit.test("Test qs_partition pivot value", function(assert){
+QUnit.test("Test qs_partition pivot value", async function(assert){
     // for now pivot is at the end of array
     var arr  = [2,3,9,9,5];
 
@@ -40,25 +58,25 @@ QUnit.test("Test qs_partition pivot value", function(assert){
     let i_end = 4;
     
 
-    let i_result = qs_partition(arr, i_start, i_end);
+    let i_result = await qs_partition(arr, i_start, i_end);
     assert.equal(i_result,2, "correct parition index");
 
     assert.ok(arr.slice(i_result,i_end).every(el => el >= arr[i_result]), "every element right is greater than pivot")
     assert.ok(arr.slice(i_start,i_result).every(el => el <= arr[i_result]), "every element left is less than pivot")
 
     arr = [2,3,5,8,3,5,6,3,67,9,3]
-    i_result = qs_partition(arr, 0, (arr.length-1));
+    i_result = await qs_partition(arr, 0, (arr.length-1));
     assert.ok(arr.slice(i_result,i_end).every(el => el >= arr[i_result]), "every element right is greater than pivot")
     assert.ok(arr.slice(i_start,i_result).every(el => el <= arr[i_result]), "every element left is less than pivot")
 
 });
 
 
-QUnit.test("Test QuickSort if sorted", function(assert){
+QUnit.test("Test QuickSort if sorted", async function(assert){
     var arr  = [3,9,9,2,5];
     var sorted_arr = [2,3,5,9,9];
 
-    QuickSort(arr,0,arr.length -1);
+    await QuickSort(arr,0,arr.length -1);
     assert.deepEqual(arr,sorted_arr, "check array");    
 });
 
